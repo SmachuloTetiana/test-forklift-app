05a47dd7fc0e572322825332f46707667ed7b4c4
Tetiana <tanichk@ukr.net>
2020-06-26 16:08:42 +0300

    added FilterPanel

---
+++ src/containers/FilterPanel/FilterPanel.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const FilterPanel = () => {
+  return (
+    <div className="toolbar mb-3">
+      <div className="toolbar-title">
+        <span>Shop By</span>
+      </div>
+
+      <div className="toolbar-content mt-4">
+        <p className="toolbar-subtitle">type</p>
+      </div>
+    </div>
+  );
+};
--- src/containers/Products/Products.js
+++ src/containers/Products/Products.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { database } from "../../firebase";
 import { AddForkliftForm } from "./AddForkliftsForm";
 import { Product } from "./Product";
+import { FilterPanel } from "containers/FilterPanel/FilterPanel";
 
 const Products = ({ getProducts, products, isLoggedIn }) => {
   useEffect(() => {
@@ -16,20 +17,27 @@ const Products = ({ getProducts, products, isLoggedIn }) => {
 
       <h2 className="title text-center mt-5 mb-5">Own production forklifts</h2>
 
-      <ul className="row">
-        {!products ? (
-          <p>No data</p>
-        ) : (
-          Object.entries(products).map(([key, val]) => (
-            <li
-              key={key}
-              className="Product col-4 d-flex flex-column pl-4 pr-4 mb-4"
-            >
-              <Product {...val} index={key} />
-            </li>
-          ))
-        )}
-      </ul>
+      <div className="row">
+        <div className="col-3">
+          <FilterPanel />
+        </div>
+        <div className="col-9">
+          <ul className="row">
+            {!products ? (
+              <p>No data</p>
+            ) : (
+              Object.entries(products).map(([key, val]) => (
+                <li
+                  key={key}
+                  className="Product col-4 d-flex flex-column pl-4 pr-4 mb-4"
+                >
+                  <Product {...val} index={key} />
+                </li>
+              ))
+            )}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
